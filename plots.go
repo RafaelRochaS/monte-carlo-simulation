511fbe6c7d5f8c02b41dfddea5ca029dc310cdf8
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2025-10-06 20:29:09 -0300

    add: set subscriber point size according to radius and print circle to show coverage area

--- plots.go
+++ plots.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"math"
 	"os"
 
 	"gonum.org/v1/plot"
@@ -80,9 +81,23 @@ func plotSubscribers(subscribers []Subscriber, dist Distribution, number int) er
 
 	s.Color = color.RGBA{R: 255, A: 255}
 	s.Shape = draw.CircleGlyph{}
-	s.Radius = vg.Points(.8)
+	s.Radius = vg.Points(-0.2*float64(number) + 2)
 
-	p.Add(s)
+	circlePoints := make(plotter.XYs, 361)
+	for i := 0; i <= 360; i++ {
+		angle := float64(i) * math.Pi / 180.0
+		circlePoints[i].X = float64(number) * math.Cos(angle)
+		circlePoints[i].Y = float64(number) * math.Sin(angle)
+	}
+
+	circle, err := plotter.NewLine(circlePoints)
+	if err != nil {
+		return err
+	}
+	circle.Color = color.RGBA{B: 255, A: 255}
+	circle.Width = vg.Points(1)
+
+	p.Add(s, circle)
 
 	if err := p.Save(10*vg.Inch, 10*vg.Inch, fmt.Sprintf("output/subscribers_%s_%d.png", dist, number)); err != nil {
 		return err
